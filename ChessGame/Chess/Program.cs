04a9a7d92b35689af73eb41308a3b71ff75d922a
alexandre <alex.gosselin04.ag59@gmail.com>
2018-11-13 21:41:12 +0100

    initial commit

---
+++ ChessGame/Chess/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuEchec
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            MyLog("Hello, Application d'échec");
+            MyLog("End, Application d'échec");
+            MyPrint("Appuyez sur une touche pour continuer");
+            Console.ReadKey();
+            return 0;
+        }
+
+        static void MyLog(string s)
+        {
+            Console.WriteLine("##### " + s);
+        }
+
+        static void MyPrint(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
