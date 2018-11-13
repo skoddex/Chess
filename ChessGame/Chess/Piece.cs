0e5c638a30a7b04704912a6dda0c33f14a8bd787
Thibault <redutils0@gmail.com>
2018-11-13 22:42:38 +0100

    ajou de piece .cs

---
+++ ChessGame/Chess/Piece.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Piece
+    {
+        public char nom { get; set; }
+        public short valeur { get; set; }
+
+        public Piece(char name)
+        {
+            nom = name;
+
+            if (name == 'D')
+            {
+                valeur = 8;
+            }
+            else if (name == 'T')
+            {
+                valeur = 5;
+            }
+            else if (name == 'P')
+            {
+                valeur = 1;
+            }
+            else
+            {
+                valeur = 3;
+            }
+        }
+    }
+}
